98a41fa66d628fc3b53c2008da13100e23b1dc9b
Josef Katič <pepca.katic@gmail.com>
2019-10-25 10:20:25 +0200

    Error message added

--- index.js
+++ index.js
@@ -10,22 +10,30 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
-app.post('/auth/login', (req, res) => {
-  const { username, password } = req.body;
-  ad.authenticate(username, password, function(err, auth) {
-    if (err) {
-      res.status(401).json({ error: 'ERROR: ' + JSON.stringify(err) });
-      return;
-    }
-    if (auth) {
-      res.status(200).json({ exist: auth, error: '' });
-    } else {
-      res
-        .status(401)
-        .json({ exist: auth, error: 'Wrong username or password!' });
-    }
-  });
-});
+app.post(
+  '/auth/login',
+  (req, res) => {
+    const { username, password } = req.body;
+    ad.authenticate(username, password, function(err, auth) {
+      if (err) {
+        res.status(401).json({ error: 'ERROR: ' + JSON.stringify(err) });
+        return;
+      }
+      if (auth) {
+        res.status(200).json({ exist: auth, error: '' });
+      } else {
+        res
+          .status(401)
+          .json({ exist: auth, error: 'Wrong username or password!' });
+      }
+    });
+  },
+  err =>
+    res.status(500).json({
+      status: 500,
+      error: err.toString()
+    })
+);
 
 app.listen(port, () => {
   console.log(`Auth Server is running on http://localhost:${port}`);
