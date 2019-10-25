794944d0d6a5bc7aaf56ca6fe7904828e9d362b1
Josef Katič <pepca.katic@gmail.com>
2019-10-25 10:10:21 +0200

    Simplified

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # ActiveDirectoryJWTAuthServer
 
 Express JWT Auth server using Active Directory
-Create private.key and public.key - 512bit - http://travistidwell.com/jsencrypt/demo/
--- index.js
+++ index.js
@@ -1,80 +1,29 @@
-require('dotenv').config();
-const jwt = require('./JWTManager');
+require('dotenv').config({ path: './config/.env' });
 const bodyParser = require('body-parser');
-const randtoken = require('rand-token');
 const cors = require('cors');
 const express = require('express');
-const app = express();
-const passport = require('./passport');
 const ad = require('./ActiveDirectoryConnector');
-const unless = require('express-unless');
 const port = 3000;
-const refreshTokens = {};
-const opts = { failWithError: true };
 
+const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(cors());
 
-const checkToken = (req, res, next) => {
-  const header = req.headers['x-access-token'] || req.headers['authorization'];
-  if (!header)
-    return res
-      .status(401)
-      .json({ message: 'Access denied. No token provided.' });
-  const bearer = header.split(' ');
-  const token = bearer[1];
-  req.token = token;
-  if (jwt.verify(token)) {
-    next();
-  } else {
-    res.status(400).json({ message: 'Invalid Token!' });
-  }
-};
-checkToken.unless = unless;
-
-app.use(
-  '/auth',
-  checkToken.unless({
-    path: ['/auth/login', '/auth/logout', '/auth/refresh']
-  })
-);
-
-app.post(
-  '/auth/login',
-  passport.authenticate('ActiveDirectory', opts),
-  (req, res) => {
-    const user = req.user;
-    const refreshToken = randtoken.uid(256);
-    refreshTokens[refreshToken] = user.username;
-    const token = jwt.sign(user);
-    res.status(200).json({
-      jwt: token,
-      refreshToken
-    });
-  },
-  function(err) {
-    res.status(401).json({ error: 'ERROR: ' + JSON.stringify(err) });
-  }
-);
-app.get('/auth/logout', (req, res) => {});
-
-app.get('/auth/refresh', function(req, res) {
-  const refreshToken = req.headers.refreshtoken;
-  if (refreshToken in refreshTokens) {
-    res.status(200).json({ jwt: jwt.sign(refreshTokens[refreshToken]) });
-  } else {
-    res.status(401);
-  }
-});
-
-app.get('/auth/profile', (req, res) => {
-  const token = jwt.decode(req.token);
-  const username = token.payload.username;
-  ad.getProfileInfo(username, user => {
-    res.status(200).json(user);
+app.post('/auth/login', (req, res) => {
+  const { username, password } = req.body;
+  ad.authenticate(username, password, function(err, auth) {
+    if (err) {
+      res.status(401).json({ error: 'ERROR: ' + JSON.stringify(err) });
+      return;
+    }
+    if (auth) {
+      res.status(200).json({ exist: auth, error: '' });
+    } else {
+      res
+        .status(401)
+        .json({ exist: auth, error: 'Wrong username or password!' });
+    }
   });
 });
 
